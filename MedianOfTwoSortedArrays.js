595fbdf5719f0a7e0727eed742b896424366ee15
Adrian <62675315+kaiseradr@users.noreply.github.com>
2022-10-23 22:14:13 -0500

    number of bug fixes and made code more concise

--- MedianOfTwoSortedArrays.js
+++ MedianOfTwoSortedArrays.js
@@ -1,58 +1,56 @@
 var findMedianSortedArrays = function (nums1, nums2) {
-    let m = nums1.length;
-    let n = nums2.length;
-    let totalLength = m + n;
+    const m = nums1.length;
+    const n = nums2.length;
+    const totalLength = m + n;
     if (!(m >= 0 && m <= 1000) || !(n >= 0 && n <= 1000) || (totalLength) < 1) {
         return -1;
     }
-    i = 0;
-    j = 0;
-    const merged = [];
-    arr1Finished = false;
-    arr2Finished = false;
+    let merged = [];  //merged array
+    if (m === 0) {
+        merged = nums2;
+    } else if (n === 0) {
+        merged = nums1;
+    } else {
+        let i = 0;
+        let j = 0;
+        let fakeElement = 0;
 
-    // begin making merged array by comparing arrays until one is finished
-    for (let ctr = 0; ctr < (totalLength); ctr++) {
-        if (nums1[i] < nums2[j]) {
-            merged[ctr] = nums1[i];
-            if (i === m - 1) {
-                arr1Finished = true;
-                break;
+        //make merged array by comparing arrays
+        for (let ctr = 0; ctr < (totalLength); ctr++) {
+            if (nums1[i] < nums2[j]) {
+                merged[ctr] = nums1[i];
+                if (i === m - 1) {
+                    fakeElement = nums2[n - 1] + 1;
+                    nums1.push(fakeElement); //ensures that this array will not be picked again, but changes original nums1 argrument which is deceiving
+                }
+                i++;
+            } else {
+                merged[ctr] = nums2[j];
+                if (j === n - 1) {
+                    fakeElement = nums1[m - 1] + 1;
+                    nums2.push(fakeElement);
+                }
+                j++;
             }
-            i++;
-        } else {
-            merged[ctr] = nums2[j];
-            if (j === n - 1) {
-                arr2Finished = true;
-                break;
-            }
-            j++;
         }
     }
 
-    //add remaining elements from array that is not finished to end of merged array
-    let remainder = [];
-    if (arr1Finished === true) {
-        remainder = nums2.slice(j);
-    } else {
-        remainder = nums1.slice(i);
+    for (let element in merged) {
+        if (element < -(10 ** 6) || element > 10 ** 6) return -1; //checking value constraints
     }
-    let mergedFinal = merged.concat(remainder);
-
-    // console.log(`merged = ${merged}`);
-    // console.log(arr3);
 
     //determine median
     if (totalLength % 2 === 0) {  //if even, return average of two innermost elements
-        medianUpper = mergedFinal[totalLength / 2];
-        medianLower = mergedFinal[medianUpper - 1];
+        medianUpper = merged[totalLength / 2];
+        medianLower = merged[totalLength / 2 - 1];
         return (medianLower + medianUpper) / 2;
     } else {
         medianIndex = Math.floor(totalLength / 2);  //if odd, return value in innermost element
-        return mergedFinal[medianIndex];
+        return merged[medianIndex];
     }
 };
 
-arr1 = [2, 3, 7, 8, 13, 15];
+
+arr1 = [2, 3, 7, 8, 13, 14];
 arr2 = [1, 4, 6, 10];
 console.log(findMedianSortedArrays(arr1, arr2));
